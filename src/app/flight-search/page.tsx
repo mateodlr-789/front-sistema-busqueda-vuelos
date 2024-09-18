e56834dc7aca29ad93dbaf916dbd83d1d3cd2a97
MrRichar03 <richarmadina03@gmail.com>
2024-09-17 21:06:50 -0500

    feat: footer was added to both pages

--- src/app/flight-search/page.tsx
+++ src/app/flight-search/page.tsx
@@ -2,8 +2,20 @@ import CardFlight from "./components/card-flight/card-flight";
 
 export default function FlightSearch() {
   return (
-    <div className="bg-white w-full px-40 h-full flex items-center justify-center dark:bg-slate-950">
-      <CardFlight/>
+    <div className="flex flex-col min-h-screen bg-white dark:bg-slate-950">
+      <main className="flex-1 px-40 py-8 flex items-center justify-center">
+        <CardFlight />
+      </main>
+      <footer className="w-full pb-14 pt-4 bg-gray-200 dark:bg-slate-800 mt-auto">
+        <div className="container mx-auto px-4 text-center">
+          <p className="text-base text-gray-700 dark:text-gray-300 mb-2">
+            Copyright © 2024 - CodeF@ctory UdeA
+          </p>
+          <p className="text-base text-gray-700 dark:text-gray-300 mb-2">
+            Términos y Condiciones
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
